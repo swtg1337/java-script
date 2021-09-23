79eb0f5bc64338b31d49dd45080fc6002f86c661
Swtg1337 <31216735+swtg1337@users.noreply.github.com>
2021-09-23 21:21:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 setInterval(function() {
     
-const today = new Date();
-const day = today.getUTCDate();
-const second  = String(today.getSeconds()).padStart(2, '0'); 
-const minutes = String(today.getMinutes()).padStart(2, '0');
-const hours = String(today.getHours()).padStart(2, '0');
-const month = today.getMonth();
-const year = today.getFullYear();
+    const today = new Date();
+    const day = today.getUTCDate();
+    const second  = today.getSeconds(); 
+    const minutes = today.getMinutes();
+    const hours = today.getHours();
+    const month = today.getMonth();
+    const year = today.getFullYear();
  
-document.getElementById("refresh").innerText = hours + ':' + minutes + ':' + second;
-document.getElementById("refresh1").innerText = day + '.' + month + '.' + year;
+    document.getElementById("time").innerText = String(hours).padStart(2, '0') + ':' + String(minutes).padStart(2, '0') + ':' + String(second).padStart(2, '0'); 
+    document.getElementById("date").innerText = day + '.' + month + '.' + year;
 }, 1000);   
